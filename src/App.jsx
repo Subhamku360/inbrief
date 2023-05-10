f47784f123990f88f3add8cbea6ad6a39c315d63
subhamku360 <21bcs4661@cuchd.in>
2023-05-11 00:34:16 +0530

    Setup done

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import NavBar from './components/NavBar';
+import News from './components/News';
+import {Route, Routes} from "react-router-dom";
+import LoadingBar from 'react-top-loading-bar'
+
+const App = ()=> {
+  const pageSize = 5;
+  const apiKey = import.meta.env.VITE_NEWS_API
+  const [progress, setProgress] = useState(0)
+ 
+    return (
+      <div>
+        <NavBar/> 
+        <LoadingBar
+        height={3}
+        color='#f11946'
+        progress={progress} 
+      />
+        <Routes>
+          <Route path="/" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"general"}/>}/>
+          <Route path="/business" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"business"}/>}/>
+          <Route path="/entertainment" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"entertainment"}/>}/>
+          <Route path="/general" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"general"}/>}/>
+          <Route path="/health" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"health"}/>}/>
+          <Route path="/science" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"science"}/>}/>
+          <Route path="/sports" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"sports"}/>}/>
+          <Route path="/technology" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"technology"}/>}/>
+        </Routes>
+      </div>
+    )
+ 
+}
+
+export default App;
