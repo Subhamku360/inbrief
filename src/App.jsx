065b77e84116e88e7509af4b03d34e8a2b130a1a
Subham Kumar <96713794+Subhamku360@users.noreply.github.com>
2023-05-10 20:11:44 +0000

    Completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,6 @@ import LoadingBar from 'react-top-loading-bar'
 
 const App = ()=> {
   const pageSize = 5;
-  const apiKey = import.meta.env.VITE_NEWS_API
   const [progress, setProgress] = useState(0)
  
     return (
@@ -18,14 +17,14 @@ const App = ()=> {
         progress={progress} 
       />
         <Routes>
-          <Route path="/" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"general"}/>}/>
-          <Route path="/business" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"business"}/>}/>
-          <Route path="/entertainment" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"entertainment"}/>}/>
-          <Route path="/general" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"general"}/>}/>
-          <Route path="/health" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"health"}/>}/>
-          <Route path="/science" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"science"}/>}/>
-          <Route path="/sports" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"sports"}/>}/>
-          <Route path="/technology" element={<News setProgress={setProgress} api={apiKey} pageSize={pageSize} country="in" category={"technology"}/>}/>
+          <Route path="/" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"general"}/>}/>
+          <Route path="/business" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"business"}/>}/>
+          <Route path="/entertainment" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"entertainment"}/>}/>
+          <Route path="/general" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"general"}/>}/>
+          <Route path="/health" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"health"}/>}/>
+          <Route path="/science" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"science"}/>}/>
+          <Route path="/sports" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"sports"}/>}/>
+          <Route path="/technology" element={<News setProgress={setProgress} pageSize={pageSize} country="in" category={"technology"}/>}/>
         </Routes>
       </div>
     )
